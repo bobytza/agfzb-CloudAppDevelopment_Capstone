d3e42d3d68e0adecf6a683807c88d1b6f8125bc9
bobytza <b.dumitrana@yahoo.com?>
2023-08-03 10:34:39 -0400

    nlu api works

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from .models import CarDealer
+from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 
 
@@ -53,12 +53,50 @@ def get_dealers_from_cf(url, **kwargs):
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
-
+def get_dealer_reviews_from_cf(url, dealerId):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url, dealerId=dealerId)
+    if json_result:
+        # Get the row list in JSON as dealers
+        reviews = json_result
+        # For each dealer object
+        for review in reviews:
+            # Get its content in `doc` object
+            #review_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            review_obj = DealerReview(dealership=review.get("dealership"), name=review.get("name"), purchase=review.get("purchase"), 
+                                    review=review.get("review"), purchase_date=review.get("purchase_date"), car_make=review.get("car_make"), 
+                                    car_model=review.get("car_model"), car_year=review.get("car_year"), sentiment=analyze_review_sentiments(review.get("review")), id=review.get("id"))
+            results.append(review_obj)
+    return results
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
+def analyze_review_sentiments(text):
+    import json
+    from ibm_watson import NaturalLanguageUnderstandingV1
+    from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+    from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
+    authenticator = IAMAuthenticator('0errok9DwW8GQ2W1Ge6CNxB3_O3WRAXi0CS6at194Seb')
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2022-04-07',
+        authenticator=authenticator)
+
+    natural_language_understanding.set_service_url('https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/60cd5457-6a8e-4e14-99c7-08ff58d9c5e4')
+
+    response = natural_language_understanding.analyze(
+        text=text,
+        features=Features(
+            entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
+            keywords=KeywordsOptions(emotion=True, sentiment=True,
+                                    limit=2))).get_result()
+
+    print(json.dumps(response, indent=2))
+    print(response["keywords"][0]["sentiment"]["label"])
+    return response["keywords"][0]["sentiment"]["label"]
 
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-from .restapis import get_dealers_from_cf
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -91,7 +91,15 @@ def get_dealerships(request):
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/be932420-f3e7-4769-a777-9aed02e58cd2/dealership-package/get-review"
+        # Get dealers from the URL
+        reviews = get_dealer_reviews_from_cf(url, dealerId=dealer_id)
+        # Concat all dealer's short name
+        reviews_names = ' --- '.join([review.name + "(" + review.sentiment + ")" for review in reviews])
+        # Return a list of dealer short name
+        return HttpResponse(reviews_names)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
